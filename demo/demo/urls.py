7c7f9a1b8421eddb8f4044a9099be81607bd596a
andrefilipe90 <andrefilipe90@gmail.com>
2024-07-23 16:53:27 -0300

    feat: polls model implementation

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -18,4 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("crimson.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("crimson.urls")),
+    path("polls/", include("polls.urls")),
+]
--- terminals.md
+++ terminals.md
@@ -1,4 +1,6 @@
 cd demo
+django-admin startproject demo
 python manage.py startapp crimson
-python manage.py makemigrations
+python manage.py make migrations
 python manage.py migrate
+python manage.py runserver
