087e91f4c9f562c107b55cc9702c95cd512c1272
andrefilipe90 <andrefilipe90@gmail.com>
2024-08-23 14:15:29 -0300

    Delete: restart project

--- crimson/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Sala
-
-class SalaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Sala
-        fields = ('id', 'nome', 'quantidade_alunos', 'status', 'criacao', 'inicio', 'final' )
